b22ddcd2faece1ac46e97fa73bae65569816a13b
ogabrielkist <gabrielkist02@gmail.com>
2025-10-06 18:51:07 -0300

    chore: remove debugging logs

--- src/controllers/controlador_pessoa.py
+++ src/controllers/controlador_pessoa.py
@@ -10,14 +10,21 @@ class ControladorPessoa:
 
     def incluir_pessoa(self):
         dados_pessoa = self.__tela_pessoa.pega_dados_pessoa()
-        
+
         for participante in self.__participantes:
             if participante.identificacao() == dados_pessoa["identificacao"]:
-                self.__tela_pessoa.mostra_mensagem("ERRO: Pessoa com essa identificação já cadastrada.")
+                self.__tela_pessoa.mostra_mensagem(
+                    "ERRO: Pessoa com essa identificação já cadastrada."
+                )
                 return
 
-        pessoa = Pessoa(dados_pessoa["nome"], dados_pessoa["celular"], dados_pessoa["identificacao"], dados_pessoa["idade"])
-    
+        pessoa = Pessoa(
+            dados_pessoa["nome"],
+            dados_pessoa["celular"],
+            dados_pessoa["identificacao"],
+            dados_pessoa["idade"],
+        )
+
         self.__participantes.append(pessoa)
         self.__tela_pessoa.mostra_mensagem("Pessoa incluída com sucesso!")
 
@@ -25,32 +32,30 @@ class ControladorPessoa:
         if not self.__participantes:
             self.__tela_pessoa.mostra_mensagem("Nenhuma pessoa cadastrada.")
             return
-        
-        print(f'PARTICIPANTES ----> {self.__participantes}')
+
         for pessoa in self.__participantes:
-            print(f'PESSOA ----> {pessoa}')
             dados_pessoa = {
                 "nome": pessoa.nome,
                 "celular": pessoa.celular,
                 "identificacao": pessoa.identificacao,
                 "idade": pessoa.idade,
             }
-    
+
             self.__tela_pessoa.mostra_pessoa(dados_pessoa)
-            
+
     def excluir_pessoa(self):
         self.listar_pessoas()
         if not self.__participantes:
             return
 
         identificacao = self.__tela_pessoa.seleciona_pessoa()
-        
+
         pessoa_encontrada = None
         for pessoa in self.__participantes:
             if pessoa.identificacao == identificacao:
                 pessoa_encontrada = pessoa
                 break
-        
+
         if pessoa_encontrada:
             self.__participantes.remove(pessoa_encontrada)
             self.__tela_pessoa.mostra_mensagem("Pessoa removida com sucesso!")
@@ -65,7 +70,7 @@ class ControladorPessoa:
             1: self.incluir_pessoa,
             2: self.listar_pessoas,
             3: self.excluir_pessoa,
-            0: self.retornar
+            0: self.retornar,
         }
 
         while True:
